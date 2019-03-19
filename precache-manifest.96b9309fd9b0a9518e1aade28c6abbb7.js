8e6c439514741547ecda540f22b3a4542fc08197
hihajdus <sebastian.hajdus@gmail.com>
2019-03-20 00:17:55 +0100

    Updates

---
+++ precache-manifest.96b9309fd9b0a9518e1aade28c6abbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5065988f3d280b83aac3",
+    "url": "/votingapp/static/js/runtime~main.5b110733.js"
+  },
+  {
+    "revision": "56b26f65964f30f5e34f",
+    "url": "/votingapp/static/js/main.5f259b86.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/votingapp/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "56b26f65964f30f5e34f",
+    "url": "/votingapp/static/css/main.4b5533b4.chunk.css"
+  },
+  {
+    "revision": "93efe82b7289e44fbd785e71f016726f",
+    "url": "/votingapp/index.html"
+  }
+];
