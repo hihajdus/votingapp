ddc06bffd92c09f85fd73689dae3b1e5c415ef4b
hihajdus <sebastian.hajdus@gmail.com>
2019-03-21 00:58:01 +0100

    complete bestbeer voting app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const List = [
+  {
+    id: 1,
+    name: 'witbir',
+    votes: 0
+  },
+  {
+    id: 2,
+    name: 'stout',
+    votes: 0
+  },
+  {
+    id: 3,
+    name: 'hefe-waizen',
+    votes: 0
+  },
+  {
+    id: 4,
+    name: 'bestbiter',
+    votes: 0
+  },
+  {
+    id: 5,
+    name: 'IPA',
+    votes: 0
+  },
+  {
+    id: 6,
+    name: 'pils',
+    votes: 0
+  }
+]
 
 class App extends Component {
-  render() {
-    return (
-      <BestAnimal>
+  state = {
+    beer: []
+  };
 
-      </BestAnimal>
-    );
+  componentDidMount() {
+    this.setState({ beer: List});
+  }
+
+  handleEvent = beerId => {
+    const updateList = this.state.beer.map(beer => {
+      if (beer.id === beerId) {
+        return Object.assign({}, beer, {
+          votes: beer.votes + 1
+        });
+      } else {
+        return beer;
+      }
+    });
+
+    this.setState({
+      beer: updateList
+    });
+  };
+
+  render() {
+    return this.state.beer.map(beer => (
+      <BestBeers
+        name={beer.name}
+        key={beer.id}
+        id={beer.id}
+        votes={beer.votes}
+        onVote={this.handleEvent}
+      />
+    ));
   }
 }
 
-class BestAnimal extends Component {
+class BestBeers extends Component {
+  handleClick = () => this.props.onVote(this.props.id);
+
   render() {
     return (
       <div className="App">
-        <p>dog</p>
-        <p>cat</p>
-        <p>bird</p>
+        <p>
+          {this.props.name}
+          <button onClick={this.handleClick}>+</button>
+          {this.props.votes}
+        </p>
       </div>
     );
   }
